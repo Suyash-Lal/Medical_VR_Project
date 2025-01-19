50d19cb33ce7a0018b428f461a7aa6fcf5bc4ba6
Suyash Lal <suyash.a.lal@gmail.com>
2025-01-20 04:15:40 +0530

    Updated UI design in scene

---
+++ Medical_Project/Assets/Suyash/Scripts/CompressionDepth.cs
@@ -0,0 +1,23 @@
+using Meta.XR.ImmersiveDebugger.UserInterface.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CompressionDepth : MonoBehaviour
+{
+    public GameObject Interactable;
+    public GameObject Affected;
+
+    private float shift_val;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
